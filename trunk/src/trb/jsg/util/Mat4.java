4832087f96e3871adda0dbe57bc85f79e34021d4
tombryntesen <tombryntesen@88dc5dda-9174-11dd-9853-59d03fe020da>
2012-01-13 12:42:56 +0000

    fixed copyright header

--- trunk/src/trb/jsg/util/Mat4.java
+++ trunk/src/trb/jsg/util/Mat4.java
@@ -1,20 +1,33 @@
 /*
- * HALDEN VR CENTRE UTILITY LIBRARY
+ * Copyright (c) 2008-2012 Java Scene Graph
+ * All rights reserved.
  *
- * J3D UTILITY CLASSES
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
  *
- * $RCSfile: Mat4.java,v $
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
  *
- * Author : Tom-Robert Bryntesen
- * Version: $Revision: 1.9 $ ($Date: 2011-12-13 13:20:37 $)
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
  *
- * (c) 1999-2010 Halden Virtual Reality Centre <http://www.ife.no/vr/>,
- * Institutt for energiteknikk. All rights reserved.
+ * * Neither the name of 'Java Scene Graph' nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
  *
- * This code is the property of Halden VR Centre <vr-info@hrp.no> and may
- * only be used in accordance with the terms of the license agreement
- * granted.
- *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package trb.jsg.util;
 
