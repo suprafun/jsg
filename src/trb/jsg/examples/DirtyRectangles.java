ec798639dee5e175a9f80caf5d7af0e77a3d89ee
tombryntesen <tombryntesen@88dc5dda-9174-11dd-9853-59d03fe020da>
2012-01-13 12:12:14 +0000

    simplified code

--- src/trb/jsg/examples/DirtyRectangles.java
+++ src/trb/jsg/examples/DirtyRectangles.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008 Java Scene Graph
+ * Copyright (c) 2008-2012 Java Scene Graph
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -13,7 +13,7 @@
  *   notice, this list of conditions and the following disclaimer in the
  *   documentation and/or other materials provided with the distribution.
  *
- * * Neither the name of 'jMonkeyEngine' nor the names of its contributors
+ * * Neither the name of 'Java Scene Graph' nor the names of its contributors
  *   may be used to endorse or promote products derived from this software
  *   without specific prior written permission.
  *
@@ -59,67 +59,30 @@ import trb.jsg.renderer.Renderer;
  */
 public class DirtyRectangles {
     public static void main(String[] args) throws Exception {
-        // Use LWJGL to create a frame
-        int windowWidth = 640;
-        int windowHeight = 480;
-        Display.setLocation((Display.getDisplayMode().getWidth() - windowWidth) / 2,
-                (Display.getDisplayMode().getHeight() - windowHeight) / 2);
-        Display.setDisplayMode(new DisplayMode(windowWidth, windowHeight));
+        Display.setDisplayMode(new DisplayMode(640, 480));
         Display.create();
 
-        // camera at origo without rotation
-        Matrix4f cameraMatrix = new Matrix4f();
-        cameraMatrix.setIdentity();
-
-        // ortho mode with a 1:1 mapping to the screen
-        View view = new View();
-        view.setCameraMatrix(cameraMatrix);
-        view.ortho(0, windowWidth, 0, windowHeight, -1000, 1000);
-
-        // create a renderpass that renders to the screen
         RenderPass renderPass = new RenderPass();
         renderPass.setClearMask(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
-        renderPass.setClearColor(new Color4f(0, 0, 0.4f, 0));
-        renderPass.setView(view);
+        renderPass.setView(View.createOrtho(0, 640, 0, 480, -1000, 1000));
 
-        // generate the texture pixels
         ByteBuffer byteBuffer = BufferUtils.createByteBuffer(256 * 256 * 4);
-
-        // create the texture using the pixels
         ByteBuffer[][] pixels = {{byteBuffer}};
         Texture texture = new Texture();
         texture.setTextureData(TextureType.TEXTURE_2D, 4, 256, 256, 0, Format.BGRA, pixels, false);
 
-        // create texture unit containing the texture
-        Unit unit = new Unit();
-        unit.setEnabled(true);
-        unit.setTexture(texture);
-
         // a simple triangle
-        VertexData vertexData = new VertexData();
-        vertexData.setCoordinates(
+        Shape shape = new Shape(new VertexData(
                 new float[]{100, 100, 0, 100, 400, 0, 400, 400, 0, 400, 100, 0} // coordinates
                 , null, null, 2
                 , new float[][]{{0, 0,  0, 1,   1, 1,   1, 0}}
                 , new int[]{0, 1, 2,  2, 3, 0} // indices
-                );
-
-        // shape has vertex data, state and matrix
-        Shape shape = new Shape();
-        shape.setVertexData(vertexData);
-
-        // set texture unit to the shapes state
-        shape.getState().setUnit(0, unit);
+                ));
+        shape.getState().setUnit(0, new Unit(texture));
 
         // add shape to the renderpass tree
-        TreeNode root = renderPass.getRootNode();
-        root.addShape(shape);
-
-        // add renderpass to scene graph
-        SceneGraph sceneGraph = new SceneGraph();
-        sceneGraph.insertRenderPass(renderPass, 0);
-
-        // create a renderer that renders the scenegraph
+        renderPass.getRootNode().addShape(shape);
+        SceneGraph sceneGraph = new SceneGraph(renderPass);
         Renderer renderer = new Renderer(sceneGraph);
 
         // main game loop
@@ -138,14 +101,8 @@ public class DirtyRectangles {
             texture.pixelsChanged(new Rectangle(20, 20, 100, 100));
             texture.pixelsChanged(new Rectangle(200, 200, 20, 20));
 
-            // render the scene graph
             renderer.render();
-
-            // flip backbuffer
             Display.update();
         }
-
-        // destroy frame when we're done
-        Display.destroy();
     }
 }
