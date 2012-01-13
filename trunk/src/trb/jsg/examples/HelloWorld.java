8472e9a133682c53732466c93544cf55ae01eb10
tombryntesen <tombryntesen@88dc5dda-9174-11dd-9853-59d03fe020da>
2012-01-13 12:12:14 +0000

    simplified code

--- trunk/src/trb/jsg/examples/HelloWorld.java
+++ trunk/src/trb/jsg/examples/HelloWorld.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2008 Java Scene Graph
+ * Copyright (c) 2008-2012 Java Scene Graph
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -13,8 +13,8 @@
  *   notice, this list of conditions and the following disclaimer in the
  *   documentation and/or other materials provided with the distribution.
  *
- * * Neither the name of 'jMonkeyEngine' nor the names of its contributors 
- *   may be used to endorse or promote products derived from this software 
+ * * Neither the name of 'Java Scene Graph' nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software
  *   without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
@@ -33,7 +33,6 @@
 package trb.jsg.examples;
 
 import javax.vecmath.Color4f;
-import javax.vecmath.Matrix4f;
 
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
@@ -42,7 +41,6 @@ import org.lwjgl.opengl.GL11;
 import trb.jsg.RenderPass;
 import trb.jsg.SceneGraph;
 import trb.jsg.Shape;
-import trb.jsg.TreeNode;
 import trb.jsg.VertexData;
 import trb.jsg.View;
 import trb.jsg.renderer.Renderer;
@@ -59,45 +57,32 @@ public class HelloWorld {
 		// Use LWJGL to create a frame
 		int windowWidth = 640;
 		int windowHeight = 480;
-		Display.setLocation((Display.getDisplayMode().getWidth() - windowWidth) / 2,
-							(Display.getDisplayMode().getHeight() - windowHeight) / 2);
 		Display.setDisplayMode(new DisplayMode(windowWidth, windowHeight));
 		Display.create();
 
-		// camera at origo without rotation
-		Matrix4f cameraMatrix = new Matrix4f();
-		cameraMatrix.setIdentity();
-
-		// ortho mode with a 1:1 mapping to the screen
-		View view = new View();
-		view.setCameraMatrix(cameraMatrix);
-		view.ortho(0, windowWidth, 0, windowHeight, -1000, 1000);
-		
 		// create a renderpass that renders to the screen
 		RenderPass renderPass = new RenderPass();
 		renderPass.setClearMask(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
 		renderPass.setClearColor(new Color4f(0, 0, 0.4f, 0));
-		renderPass.setView(view);
-
-		// a simple triangle
-		VertexData vertexData = new VertexData();
-		vertexData.setCoordinates(
-			  new float[] {100, 100, 0,   100, 400, 0,  400, 400, 0} // coordinates
-			, null, null, 0, null
-			, new int[] {0, 1, 2} // indices
-		);
+        
+        // ortho mode with a 1:1 mapping to the screen
+		renderPass.setView(View.createOrtho(0, windowWidth, 0, windowHeight, -1000, 1000));
 
 		// shape has vertex data, state and matrix
-		Shape shape = new Shape();
-		shape.setVertexData(vertexData);
+		Shape shape = new Shape(new VertexData(
+			  new float[] {100, 100, 0,   100, 400, 0,  400, 400, 0} // coordinates
+			, null // normals
+            , null // colors
+            , 2 // texture element size
+            , null // texture coordinates
+			, new int[] {0, 1, 2} // indices
+		));
 		
 		// add shape to the renderpass tree
-		TreeNode root = renderPass.getRootNode();
-		root.addShape(shape);
+		renderPass.getRootNode().addShape(shape);
 
-		// add renderpass to scene graph
-		SceneGraph sceneGraph = new SceneGraph();
-		sceneGraph.insertRenderPass(renderPass, 0);
+		// create scene graph that uses render pass
+		SceneGraph sceneGraph = new SceneGraph(renderPass);
 
 		// create a renderer that renders the scenegraph
 		Renderer renderer = new Renderer(sceneGraph);
